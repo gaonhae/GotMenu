eb6b7f97c51d03e49da4939f67957d2e0dc7a731
gaonhae <codegaonhae@gmail.com>
2022-07-16 10:56:53 +0900

    import project starter

---
+++ src/main/java/org/TUK/gotMenu/GotMenuApplication.java
@@ -0,0 +1,13 @@
+package org.TUK.gotMenu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GotMenuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GotMenuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/TUK/gotMenu/GotMenuApplicationTests.java
@@ -0,0 +1,13 @@
+package org.TUK.gotMenu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GotMenuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
