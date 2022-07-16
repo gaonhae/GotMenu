8800c3260d3311108979847635ebe4a45ce40030
gaonhae <codegaonhae@gmail.com>
2022-07-16 12:32:03 +0900

    exclude DB

--- src/main/java/org/TUK/gotMenu/GotMenuApplication.java
+++ src/main/java/org/TUK/gotMenu/GotMenuApplication.java
@@ -2,8 +2,10 @@ package org.TUK.gotMenu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class GotMenuApplication {
 
 	public static void main(String[] args) {
