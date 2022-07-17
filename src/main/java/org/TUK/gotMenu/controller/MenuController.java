d329ab3e03f84c677a05632b64d82821f5e3f5c2
gaonhae <codegaonhae@gmail.com>
2022-07-17 15:05:18 +0900

    read menuList

--- src/main/java/org/TUK/gotMenu/controller/MenuController.java
+++ src/main/java/org/TUK/gotMenu/controller/MenuController.java
@@ -1,6 +1,28 @@
 package org.TUK.gotMenu.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.TUK.gotMenu.entity.Menu;
+import org.TUK.gotMenu.service.MenuService;
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@RequestMapping("/menu")
+@Controller
+@RequiredArgsConstructor
 public class MenuController {
+
+    private final MenuService menuService;
+
+    @RequestMapping("/list")
+    public String list(Model model, @RequestParam(value="page", defaultValue="0") int page){
+        Page<Menu> paging = this.menuService.getList(page);
+        model.addAttribute("paging", paging);	//템플릿에 페이징에 대한 내용을 넘겨줌
+        return "menuList";
+    }
+
 
 
 
--- src/main/java/org/TUK/gotMenu/service/MenuService.java
+++ src/main/java/org/TUK/gotMenu/service/MenuService.java
@@ -3,7 +3,14 @@ package org.TUK.gotMenu.service;
 import lombok.RequiredArgsConstructor;
 import org.TUK.gotMenu.entity.Menu;
 import org.TUK.gotMenu.repository.MenuRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -19,6 +26,13 @@ public class MenuService {
         m.setMenuRating(menuRating);
         m.setTags(tags);
         this.menuRepository.save(m);
+    }
+
+    public Page<Menu> getList(int page){	//MenuList 템플릿에 넘겨 페이징을 위해 사용
+        List<Sort.Order> sorts = new ArrayList<>();
+        sorts.add(Sort.Order.desc("menuNo"));
+        Pageable pageable = PageRequest.of(page, 30, Sort.by(sorts));
+        return this.menuRepository.findAll(pageable);
     }
 
 }
