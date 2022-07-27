555982c9552fbbd0d3a96981e9ef1a7310910a21
gaonhae <codegaonhae@gmail.com>
2022-07-28 01:25:55 +0900

    debug : Fix random menu

--- src/main/java/org/TUK/gotMenu/controller/MainController.java
+++ src/main/java/org/TUK/gotMenu/controller/MainController.java
@@ -1,16 +1,42 @@
 package org.TUK.gotMenu.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.TUK.gotMenu.service.MenuService;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpServletResponse;
+
 @Controller
 @RequiredArgsConstructor
-public class MainController
-{
+@EnableScheduling
+@Component
+public class MainController {
+
+    private final MenuService menuService;
+    String todayMenu = "";
     @RequestMapping("/")
     public String index()
     {
         return "index";
+    }
+
+    @RequestMapping(value = "/random",produces = "application/text; charset=UTF-8")
+    public void random(HttpServletResponse response) {
+        try {
+            response.setCharacterEncoding("UTF-8");
+            response.setContentType("application/json");
+            response.getWriter().println(todayMenu);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Scheduled(fixedDelay = 5000)  //5초에 한 번씩 추천 메뉴를 바꿈
+    public void chooseMenu() {
+        todayMenu = menuService.randomMenu();
     }
 }
